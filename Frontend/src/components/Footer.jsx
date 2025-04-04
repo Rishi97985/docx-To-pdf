be3d45ab9139e800dcd4074399d41aa00427ed0b
Rishi Gupta <gupta.rishi707@gmail.com>
2025-04-04 11:52:02 +0530

    Made few changes

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="fixed bottom-0 w-full bg-white text-center z-50 border-t border-black">
+      <h1 className="py-3 text-sm">Created by Rishi</h1>
+    </div>
+  );
+}
+
+export default Footer;
